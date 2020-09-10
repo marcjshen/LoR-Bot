53ec96040eb6dfb3c4075cd02c9d4f330d7f0095
Marc Shen <marcjshen@gmail.com>
2020-09-09 22:54:20 -0500

    Started discord bot

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+client.login(config.BOT_TOKEN);
+
+const prefix = "!";
+
+client.on("message", function(message) { 
+    if (message.author.bot) return;
+
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "help") {
+        message.reply("there's no helping your hardstuck iron bum ass");
+      }
+                                         
+});                                      
+
+client.login(config.BOT_TOKEN);
