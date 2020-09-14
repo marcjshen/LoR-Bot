58f4260f9027f3273dfd0c9ad9e13cd662b69a6c
Marc Shen <marcjshen@gmail.com>
2020-09-13 23:03:42 -0500

    Implemented summoner level search

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-
 const client = new Discord.Client();
-const API_KEY = 'RGAPI-edb08c7e-f3d9-4db8-a42f-f39aa2594ae4';
+
+const API_KEY = '';
+
+
+// var jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
+// const { window } = new JSDOM();
+// const { document } = (new JSDOM('')).window;
+// global.document = document;
+
+// var $ = jQuery = require('jquery')(window);
+
+const fetch = require("node-fetch");
 
 client.login(config.BOT_TOKEN);
 
 const prefix = "!"; // used to indicate bot command
-
-
-var jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
-global.document = document;
-
-var $ = jQuery = require('jquery')(window);
  
-
-
-/**
- * should prob make a function for getting data n ugly stuff
- */
-
-
 client.on("message", function(message) { 
     if (message.author.bot) return;
 
@@ -33,63 +28,47 @@ client.on("message", function(message) {
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
-    if (command === "help") {
-      message.reply("use !lol help, !tft help, or !val help for game-specific commands");
-    } else if (command === "lol") {
-      if (args[0] == "help") {
-        
-        var SUMMONER_NAME = "";
-        SUMMONER_NAME = "Doublelift";
-
-        var API_KEY = "";
-        API_KEY = "RGAPI-edb08c7e-f3d9-4db8-a42f-f39aa2594ae4";
-
-        if (SUMMONER_NAME !== "") {
-
-            $.ajax({
-                url: 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + 'Nehscram' + '?api_key=' + API_KEY,
-                type: 'GET',
-                dataType: 'json',
-                data: {
-
-                },
-                success: function (json) {
-                    //var SUMMONER_NAME_NOSPACES = SUMMONER_NAME.replace(" ", "");
-
-                    //SUMMONER_NAME_NOSPACES = SUMMONER_NAME_NOSPACES.toLowerCase().trim();
-
-                    //summonerLevel = json[SUMMONER_NAME_NOSPACES].summonerLevel;
-                    //summonerID = json[SUMMONER_NAME_NOSPACES].id;
-
-                    message.reply("yes");
-
-                },
-                error: function (XMLHttpRequest, textStatus, errorThrown) {
-                    message.reply("no");
-                }
-            });
-        } else {
-          message.reply("fds");;
-        }
-
-        
-        message.reply("placeholder text");
-      } 
-
-    } else if (command === "tft") {
-      if (args[0] == "help") {
-        message.reply("placeholder text");
-      } 
-
-    } else if (command === "val") {
-      if (args[0] == "help") {
-        message.reply("placeholder text");
-      } 
-
+    if (command === "help" && args.length == 0) {
+      message.reply("use `!lol help`, `!tft help`, or `!val help` for game-specific commands");
+      
     } 
 
-    
-                                         
+    else if (command === "lol") {
+      if (args[0] == "help" && args.length == 1) {
+        message.reply("available: summonerlevel(username)");
+      } 
+      else if (args[0] == "summonerlevel" && args.length == 2) {
+        var username = args[1];
+        fetch('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + username + '?api_key=' + API_KEY)
+          .then(response => response.json())
+          .then(data => message.reply('**' + username + "**'s Summonor Level: " + data.summonerLevel));
+      }
+      else {
+        message.reply ("unrecognized command or arguments, use `!help` for a list of commands.")
+      }
+    } 
+
+    else if (command === "tft") {
+      if (args[0] == "help") {
+        message.reply("placeholder text");
+      } 
+      else {
+        message.reply ("unrecognized command or arguments, use `!help` for a list of commands.")
+      }
+    } 
+
+    else if (command === "val") {
+      if (args[0] == "help") {
+        message.reply("placeholder text");
+      } 
+      else {
+        message.reply ("unrecognized command or arguments, use `!help` for a list of commands.")
+      }
+
+    } 
+    else {
+      message.reply ("unrecognized command or arguments, use `!help` for a list of commands.")
+    }                               
 });                                      
 
 client.login(config.BOT_TOKEN);
